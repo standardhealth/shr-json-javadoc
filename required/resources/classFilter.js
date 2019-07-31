9dc63549961200270101287945dff8f194b6a4fd
Mint Thompson <mathompson@mitre.org>
2019-07-31 10:15:00 -0400

    Filter classes in list.
    Add radio buttons for showing each class that is an Entry, a Group, or an Element.
    Add text input for filtering classes by name. The input string must appear somewhere within the class name. The comparison is case insensitive.
    Change header text on class list to reflect current namespace and selected class filter radio button.
    Header text in class list no longer contains a namespace link. The link wasn't working correctly anyway.
    Change namespace list to display radio buttons in order to provide an additional visual reminder of which namespace is selected.
    Namespaces in list are styled the same as the "All Namespaces" text.

--- required/resources/classFilter.js
+++ required/resources/classFilter.js
@@ -1,8 +1,18 @@
 $(window).on('load', function() {
-  $('input#class-filter').on('keyup', function() {
-    var matchText = $(this).val().toLowerCase();
+  function applyFilter() {
+    var matchText = $('input#class-name-filter').val().toLowerCase();
+    var classFilter = $('input[name=class-type-filter]:checked').val();
     var toShow = $('ul li').hide().filter(function(index, li) {
-      return $(li).find('a').text().toLowerCase().includes(matchText);
+      li = $(li);
+      if(classFilter == 'entry' && !(li.hasClass('entry') || li.hasClass('abstract'))) {
+        return false;
+      } else if(classFilter == 'group' && !li.hasClass('group')) {
+        return false;
+      } else if(classFilter == 'element' && !li.hasClass('element')) {
+        return false;
+      } else {
+        return li.find('a').text().toLowerCase().includes(matchText);
+      }
     });
     if(toShow.length == 0) {
       $('p#no-match').show();
@@ -10,5 +20,13 @@ $(window).on('load', function() {
       $('p#no-match').hide();
       toShow.show();
     }
-  });
+  }
+
+  function updateShownClasses() {
+    $('span#shown-classes').text($(this).parent().text());
+  }
+
+  $('input#class-name-filter').on('keyup', applyFilter);
+  $('input[name=class-type-filter]:radio').on('change', applyFilter);
+  $('input[name=class-type-filter]:radio').on('change', updateShownClasses);
 });
